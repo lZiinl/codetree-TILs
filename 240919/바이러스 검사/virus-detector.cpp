83b709cf31d856196bed5b456db443b572dfd39c
lZiinl <149471946+lZiinl@users.noreply.github.com>
2024-09-19 17:34:29 +0900

    Title: 바이러스 검사, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240919/바이러스 검사/virus-detector.cpp
@@ -0,0 +1,58 @@
+#define CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> store;
+int N;
+
+int man, ser;
+
+long long result;
+
+void input() {
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++){
+		int num;
+		cin >> num;
+		store.push_back(num);
+	}
+
+	cin >> man >> ser;
+
+}
+
+void solve() {
+
+	for (int i = 0; i < N; i++){
+
+		int now = store[i] - man;
+		int res = 1;
+
+		if (now > 0) {
+			res = res + now / ser;
+			res += 1;
+		}
+
+		result += res;
+	}
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	//freopen("sample.txt", "r", stdin);
+
+	input();
+	solve();
+
+	cout << result;
+
+	return 0;
+}
