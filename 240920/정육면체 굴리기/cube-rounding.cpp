57832dead7db435a7c0cb733094f6011eb5ba92c
lZiinl <149471946+lZiinl@users.noreply.github.com>
2024-09-20 14:07:03 +0900

    Title: 정육면체 굴리기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240920/정육면체 굴리기/cube-rounding.cpp
+++ 240920/정육면체 굴리기/cube-rounding.cpp
@@ -1,72 +1,51 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <vector>
+#include<iostream>
 
 using namespace std;
 
-int N, M, X, Y, K;
+int N, M, K;
+int sp_x, sp_y;
 int map[21][21];
 int cmd[1001];
+int dy[4] = { 1, 0, 0, -1 };
+int dx[4] = { 0, 1, -1, 0 };
 int dice[4][4];
-int dy[5] = { 0, 0, 0, -1, 1 };
-int dx[5] = { 0, 1, -1, 0, 0 };
 
 void input() {
+	cin >> N >> M >> sp_y >> sp_x >> K;
 
-	cin >> N >> M >> X >> Y >> K;
-
-	for (int i = 0; i < N; i++){
-		for (int j = 0; j < M; j++){
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
 			cin >> map[i][j];
 		}
 	}
 
-	for (int i = 0; i < K; i++){
+	for (int i = 0; i < K; i++) {
 		cin >> cmd[i];
 	}
 }
 
 void solve() {
 
-	for (int i = 0; i < K; i++){
+	int y = sp_y;
+	int x = sp_x;
 
-		int nx = X + dx[cmd[i]];
-		int ny = Y + dy[cmd[i]];
+	for (int i = 0; i < K; i++) {
+		int dir = (cmd[i] % 4);
 
-		if (nx < 0 || ny < 0 || nx >= M || ny >= N) continue;
+		int ny = y + dy[dir];
+		int nx = x + dx[dir];
+
+		//벽이면 가만히 있음
+		if (nx < 0 || nx >= M || ny < 0 || ny >= N) {
+			continue;
+		}
 
 		int temp = 0;
 
-		if (map[ny][nx] == 0) {
-
-			if (cmd[i] == 1) {
-				map[ny][nx] = dice[1][2];
-				temp = dice[1][0];
-				dice[1][0] = dice[1][1];
-				dice[1][1] = map[ny][nx];
-				dice[1][2] = dice[1][3];
-				dice[1][3] = temp;
-				dice[3][1] = dice[1][3];
-			}
-			else if (cmd[i] == 2) {
-				map[ny][nx] = dice[1][0];
-				temp = dice[1][3];
-				dice[1][3] = dice[1][2];
-				dice[1][2] = dice[1][1];
-				dice[1][1] = map[ny][nx];
-				dice[1][0] = temp;
-				dice[3][1] = dice[1][3];
-			}
-			else if (cmd[i] == 3) {
-				map[ny][nx] = dice[0][1];
-				temp = dice[3][1];
-				dice[3][1] = dice[2][1];
-				dice[2][1] = dice[1][1];
-				dice[1][1] = map[ny][nx];
-				dice[0][1] = temp;
-				dice[1][3] = dice[3][1];
-			}
-			else if (cmd[i] == 4) {
+		//남쪽으로 움직이면
+		if (dir == 0) {
+			if (map[ny][nx] == 0) {
 				map[ny][nx] = dice[2][1];
 				temp = dice[0][1];
 				dice[0][1] = dice[1][1];
@@ -75,38 +54,7 @@ void solve() {
 				dice[3][1] = temp;
 				dice[1][3] = dice[3][1];
 			}
-
-		}
-		else {
-
-			if (cmd[i] == 1) {
-				temp = dice[1][0];
-				dice[1][0] = dice[1][1];
-				dice[1][1] = map[ny][nx];
-				map[ny][nx] = 0;
-				dice[1][2] = dice[1][3];
-				dice[1][3] = temp;
-				dice[3][1] = dice[1][3];
-			}
-			else if (cmd[i] == 2) {
-				temp = dice[1][3];
-				dice[1][3] = dice[1][2];
-				dice[1][2] = dice[1][1];
-				dice[1][1] = map[ny][nx];
-				map[ny][nx] = 0;
-				dice[1][0] = temp;
-				dice[3][1] = dice[1][3];
-			}
-			else if (cmd[i] == 3) {
-				temp = dice[3][1];
-				dice[3][1] = dice[2][1];
-				dice[2][1] = dice[1][1];
-				dice[1][1] = map[ny][nx];
-				map[ny][nx] = 0;
-				dice[0][1] = temp;
-				dice[1][3] = dice[3][1];
-			}
-			else if (cmd[i] == 4) {
+			else {
 				temp = dice[0][1];
 				dice[0][1] = dice[1][1];
 				dice[1][1] = map[ny][nx];
@@ -115,25 +63,86 @@ void solve() {
 				dice[3][1] = temp;
 				dice[1][3] = dice[3][1];
 			}
-
 		}
 
-		X = nx;
-		Y = ny;
+		//동쪽
+		if (dir == 1) {
+			if (map[ny][nx] == 0) {
+				map[ny][nx] = dice[1][2];
+				temp = dice[1][0];
+				dice[1][0] = dice[1][1];
+				dice[1][1] = map[ny][nx];
+				dice[1][2] = dice[1][3];
+				dice[1][3] = temp;
+				dice[3][1] = dice[1][3];
+			}
+			else {
+				temp = dice[1][0];
+				dice[1][0] = dice[1][1];
+				dice[1][1] = map[ny][nx];
+				map[ny][nx] = 0;
+				dice[1][2] = dice[1][3];
+				dice[1][3] = temp;
+				dice[3][1] = dice[1][3];
+			}
+		}
 
+		//서쪽
+		if (dir == 2) {
+			if (map[ny][nx] == 0) {
+				map[ny][nx] = dice[1][0];
+				temp = dice[1][3];
+				dice[1][3] = dice[1][2];
+				dice[1][2] = dice[1][1];
+				dice[1][1] = map[ny][nx];
+				dice[1][0] = temp;
+				dice[3][1] = dice[1][3];
+			}
+			else {
+				temp = dice[1][3];
+				dice[1][3] = dice[1][2];
+				dice[1][2] = dice[1][1];
+				dice[1][1] = map[ny][nx];
+				map[ny][nx] = 0;
+				dice[1][0] = temp;
+				dice[3][1] = dice[1][3];
+			}
+		}
+
+		//북쪽
+		if (dir == 3) {
+			if (map[ny][nx] == 0) {
+				map[ny][nx] = dice[0][1];
+				temp = dice[3][1];
+				dice[3][1] = dice[2][1];
+				dice[2][1] = dice[1][1];
+				dice[1][1] = map[ny][nx];
+				dice[0][1] = temp;
+				dice[1][3] = dice[3][1];
+			}
+			else {
+				temp = dice[3][1];
+				dice[3][1] = dice[2][1];
+				dice[2][1] = dice[1][1];
+				dice[1][1] = map[ny][nx];
+				map[ny][nx] = 0;
+				dice[0][1] = temp;
+				dice[1][3] = dice[3][1];
+			}
+			
+		}
+
+		x = nx;
+		y = ny;
 		cout << dice[3][1] << "\n";
 	}
 }
 
 int main() {
-
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-
 	//freopen("sample.txt", "r", stdin);
 
 	input();
+
 	solve();
 
 	return 0;
