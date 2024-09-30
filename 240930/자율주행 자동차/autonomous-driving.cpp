0aa81e0f165c608827b167a06efdf3ed431e69a5
lZiinl <149471946+lZiinl@users.noreply.github.com>
2024-09-30 21:55:36 +0900

    Title: 자율주행 자동차, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240930/자율주행 자동차/autonomous-driving.cpp
@@ -0,0 +1,101 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int N, M;
+int X, Y, D;
+int map[51][51];
+int vis[51][51];
+
+int dx[4] = { 0, 1, 0, -1 };
+int dy[4] = { -1, 0, 1, 0 };
+
+int cnt = 1;
+
+struct Node{
+	int y, x;
+};
+
+void input() {
+	cin >> N >> M;
+	cin >> Y >> X >> D;
+
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j < M; j++){
+			cin >> map[i][j];
+		}
+	}
+}
+
+void move() {
+
+	map[Y][X] = 2;
+
+	int nx = 0;
+	int ny = 0;
+
+	queue<Node> q;
+	q.push({ Y,X });
+
+	while (!q.empty()) {
+
+		Node now = q.front();
+
+		q.pop();
+		int flag = 0;
+
+		for (int i = 0; i < 4; i++) {
+
+			D = (D + 3) % 4;
+
+			nx = now.x + dx[D];
+			ny = now.y + dy[D];
+
+			if (nx < 0 || ny < 0 || nx >= M || ny >= N) continue;
+			if (map[ny][nx] != 0) continue;
+
+			q.push({ ny,nx });
+			map[ny][nx] = 2;
+			flag = 1;
+			cnt++;
+			break;
+		}
+
+		if (flag == 1) continue;
+
+		nx = now.x - dx[D];
+		ny = now.y - dy[D];
+
+		if (nx < 0 || ny < 0 || nx >= M || ny >= N) return;
+
+		q.push({ ny,nx });
+	}
+
+}
+
+void cal() {
+	cout << cnt;
+}
+
+void solve() {
+
+	move();
+	cal();
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	//freopen("sample.txt", "r", stdin);
+
+	input();
+	solve();
+
+	return 0;
+}
