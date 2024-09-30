b4927b1a86f5ff485415a8f7115e1f080100e1df
lZiinl <149471946+lZiinl@users.noreply.github.com>
2024-09-30 22:39:51 +0900

    Title: 테트리스 블럭 안의 합 최대화 하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240930/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.cpp
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int N, M;
+int map[201][201];
+int vis[201][201];
+
+int mmax;
+
+int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+
+void input() {
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j < M; j++){
+			cin >> map[i][j];
+		}
+	}
+}
+
+void dfs(int level, int y, int x, int sum) {
+
+	if (level == 4) {
+		if (sum > mmax) {
+			mmax = sum;
+		}
+		return;
+	}
+
+	int nx = 0;
+	int ny = 0;
+
+	for (int i = 0; i < 4; i++){
+
+		nx = x + dx[i];
+		ny = y + dy[i];
+
+		if (nx < 0 || ny < 0 || nx >= M || ny >= N) continue;
+		if (vis[ny][nx] == 1) continue;
+
+		int s = sum + map[ny][nx];
+
+		vis[ny][nx] = 1;
+		dfs(level + 1, ny, nx, s);
+		vis[ny][nx] = 0;
+	}
+}
+
+void solve() {
+
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j < M; j++){
+			vis[i][j] = 1;
+			dfs(1, i, j, map[i][j]);
+			vis[i][j] = 0;
+		}
+	}
+
+	cout << mmax;
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	//freopen("sample.txt", "r", stdin);
+
+	input();
+	solve();
+
+	return 0;
+}
