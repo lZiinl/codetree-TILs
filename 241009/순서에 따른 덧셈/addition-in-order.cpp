bd1747723be3875722c3735673ed5b48a5552dae
lZiinl <149471946+lZiinl@users.noreply.github.com>
2024-10-09 11:05:12 +0900

    Title: 순서에 따른 덧셈, Time: 40ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241009/순서에 따른 덧셈/addition-in-order.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SERCURE_NO_WARNINGS
+#include <iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int N;
+vector<int>num;
+
+void input() {
+	cin >> N;
+
+	for (int i = 0; i < N; i++){
+		int temp;
+		cin >> temp;
+		num.push_back(temp);
+	}
+
+	sort(num.begin(), num.end(), greater<>());
+}
+
+void solve() {
+
+	int result = 0;
+
+	for (int i = 0; i < N; i++){
+		int sum = num[i] - i;
+
+		if (sum > 0) {
+			result += sum;
+		}
+	}
+
+	cout << result;
+}
+
+int main() {
+
+	//freopen("sample.txt", "r", stdin);
+
+	input();
+	solve();
+
+	return 0;
+}
